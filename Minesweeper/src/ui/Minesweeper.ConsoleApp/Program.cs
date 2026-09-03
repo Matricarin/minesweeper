62012fddfecc0e3c2e20036cd65d4f6c5f6ae8a1
matricarin <matricarin@gmail.com>
2026-09-03 12:22:27 +0500

    определена структура решения

---
+++ Minesweeper/src/ui/Minesweeper.ConsoleApp/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Minesweeper.ConsoleApp;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ Minesweeper/test/Minesweeper.Test/InitTests.cs
@@ -0,0 +1,10 @@
+﻿namespace Minesweeper.Test;
+
+public class InitTests
+{
+    [Test]
+    public void Init_Success()
+    {
+        Assert.That(true, Is.True);
+    }
+}
